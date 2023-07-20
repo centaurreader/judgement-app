7582202ac0c97199e0b8f0253922436ccc16514f
Paul Browning <centaur@centaurreader.com>
2023-07-19 22:25:48 -0500

    adds game view basics

---
+++ src/components/ChampionCardPreview.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Champion } from '../types/judgement';
+import style from './ChampionCardPreview.css';
+import StatBlock from './StatBlock';
+import StatControl from './StatControl';
+import ModalLink from './ModalLink';
+
+function ChampionCardPreview({
+  champion,
+}: {
+  champion: Champion;
+}) {
+  return (
+    <div className={style.container}>
+      <div className={style.details}>
+        <img
+          className={style.image}
+          src={`https://hallofeternalchampions.com/images/${champion.imageUrl}`}
+          alt=""
+        />
+
+        <div>
+          <p className={style.name}>{champion.name}</p>
+
+          <div>
+            <ul className={style.statList}>
+              <li className={style.statListItem}>
+                <StatBlock name="Mov" value={champion.mov} />
+              </li>
+              <li className={style.statListItem}>
+                <StatBlock name="Agi" value={champion.agi} />
+              </li>
+              <li className={style.statListItem}>
+                <StatBlock name="Res" value={champion.res} />
+              </li>
+              <li className={style.statListItem}>
+                <StatBlock name="Mel" value={champion.mel} />
+              </li>
+              <li className={style.statListItem}>
+                <StatBlock name="Mag" value={champion.mag} />
+              </li>
+              <li className={style.statListItem}>
+                <StatBlock name="Rng" value={champion.rng} />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.controls}>
+        <div className={style.control}>
+          <StatControl label="Level" defaultValue={0} />
+        </div>
+        <div className={style.control}>
+          <StatControl label="Health" defaultValue={0} />
+        </div>
+      </div>
+
+      <ModalLink to={`champion,${champion.id}`}>View</ModalLink>
+    </div>
+  );
+}
+
+export default ChampionCardPreview;
--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,49 +1,71 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { useGame } from '../hooks/useGame';
 import Layout from '../components/Layout';
 import { useJudgementApi } from '../hooks/useJudgementApi';
+import ChampionCardPreview from '../components/ChampionCardPreview';
 import ChampionCard from '../components/ChampionCard';
 
 function GameRoute() {
   const { data } = useJudgementApi();
   const { gameState } = useGame();
+
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  const modals = (query.get('modals')?.split('|')
+    .map((val) => val.split(','))) ?? [];
+
+  const championModals = modals.filter((modal) => modal[0] === 'champion');
+
   return (
     <Layout
-      detailChildren={(<div />)}
-      isDetailVisible
+      detailChildren={championModals.map(([, id]) => {
+        const champion = data.champions?.find((c) => c.id === id);
+        if (!champion) { return null; }
+        return (
+          <ChampionCard
+            key={champion.id}
+            activeAbilities={champion.activeAbilities}
+            combatManoeuvres={champion.combatManoeuvres}
+            commonInnateAbilities={champion.commonInnateAbilities.map((cia) => {
+              const dataCia = data.commonInnateAbilities?.find(
+                (d) => d.name.toUpperCase() === cia.toUpperCase(),
+              );
+              return {
+                name: cia,
+                description: dataCia?.description ?? 'No info available',
+              };
+            })}
+            imageUrl={champion.imageUrl}
+            name={champion.name}
+            soulHarvest={champion.soulHarvest}
+            stats={[
+              { label: 'Mov', value: champion.mov },
+              { label: 'Rng', value: champion.rng },
+              { label: 'Agi', value: champion.agi },
+              { label: 'Mel', value: champion.mel },
+              { label: 'Mag', value: champion.mag },
+              { label: 'Res', value: champion.res },
+            ]}
+            uniqueInnateAbilities={champion.uniqueInnateAbilities}
+            weapons={champion.weapons}
+            maxhp={champion.health}
+          />
+        );
+      })}
+      isDetailVisible={false}
     >
       <div>
-        {JSON.stringify(gameState)}
+        <p style={{ fontSize: '.5rem', wordBreak: 'break-word' }}>{JSON.stringify(gameState)}</p>
         {gameState.config?.player1.championIds.map((cId) => {
           const champion = data.champions?.find((c) => c.id === cId);
           if (!champion) { return null; }
           return (
-            <ChampionCard
-              activeAbilities={champion.activeAbilities}
-              combatManoeuvres={champion.combatManoeuvres}
-              commonInnateAbilities={champion.commonInnateAbilities.map((cia) => {
-                const dataCia = data.commonInnateAbilities?.find((d) => d.name === cia);
-                return {
-                  name: cia,
-                  description: dataCia?.description ?? 'No info available',
-                };
-              })}
-              imageUrl={champion.imageUrl}
-              maxhp={champion.health}
-              name={champion.name}
-              soulHarvest={champion.soulHarvest}
-              uniqueInnateAbilities={champion.uniqueInnateAbilities}
-              weapons={champion.weapons}
-              stats={[
-                { label: 'Mov', value: champion.mov },
-                { label: 'Rng', value: champion.rng },
-                { label: 'Agi', value: champion.agi },
-                { label: 'Mel', value: champion.mel },
-                { label: 'Mag', value: champion.mag },
-                { label: 'Res', value: champion.res },
-              ]}
-              key={champion.id}
-            />
+            <div className="layout-margin--top_100" key={champion.id}>
+              <ChampionCardPreview
+                champion={champion}
+              />
+            </div>
           );
         })}
       </div>
